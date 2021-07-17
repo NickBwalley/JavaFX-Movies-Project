09f83713bd1914d2d2d6ee2f970db7b6c1e06190
NickBwalley <nickbwalley@gmail.com>
2021-07-17 17:17:19 +0300

    ExprobeNetworks

---
+++ src/exprobenetworks/ExprobeNetworks.java
@@ -0,0 +1,136 @@
+package exprobenetworks;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author a47
+ */
+public class ExprobeNetworks extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        //LABELS
+    Text uname_label=new Text("UserName"); 
+    Text pass_label=new Text("Password");
+   
+    
+    //TEXTFIELD
+    TextField user_name_tf=new TextField();
+        
+    //PASSWORDFIELD
+    PasswordField pass_field = new PasswordField();
+    
+    //BUTTON
+    Button login_button = new Button("Login");
+    Button register_button = new Button("Register");
+    Button close_button = new Button("Close");
+    
+    
+    //Event Handler for the login_button    
+    login_button.setOnMouseClicked((new EventHandler<MouseEvent>() { 
+    public void handle(MouseEvent event) {        
+        String username = user_name_tf.getText();
+        String password = pass_field.getText();
+        
+        try{
+          Class.forName("com.mysql.cj.jdbc.Driver"); //step one
+
+
+          Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx?autoReconnect=true&useSSL=false","root","");  //step two
+          
+          Statement st=con.createStatement();   //step three
+          String statement = "SELECT * FROM exprobe_networks.users Where username = '"+username+"' AND password = '"+password+"' ";
+          ResultSet rs = st.executeQuery(statement); //step four
+          System.out.println(rs);
+          
+          if(rs.next()){                   
+                    Alert al = new Alert(Alert.AlertType.CONFIRMATION);
+                    al.setContentText("Successful Login");
+                    al.show();
+                    //You can add the code to open HomePage (after successful login)
+                    
+                    
+                }else{
+                    Alert a = new Alert(Alert.AlertType.WARNING);
+                    a.setContentText("Invalid User Name or Password");
+                    a.show(); 
+                }
+          con.close();
+            
+        }
+        catch(Exception ee){System.out.println(ee);System.out.println("Connection error");} 
+     
+        
+        }
+    })); 
+    
+    //creating an instance of an object of the Register.java
+    Register registration_page = new Register();
+    
+    register_button.setOnMouseClicked((new EventHandler<MouseEvent>(){
+        public void handle(MouseEvent Event){
+            registration_page.start(registration_page.register_page);
+        }
+    }));
+    
+    //close button for close the window for stage
+    close_button.setOnMouseClicked((new EventHandler<MouseEvent>(){
+        public void handle(MouseEvent event){
+            primaryStage.close();
+        }
+    }));
+   
+  //GRIDPANE
+    GridPane gridPane = new GridPane();
+    gridPane.add(uname_label,1,1);
+    gridPane.add(pass_label,1,2);    
+    gridPane.add(user_name_tf,2,1);
+    gridPane.add(pass_field,2,2);
+    gridPane.add(login_button,2,3);
+    gridPane.add(register_button,2,4);
+    gridPane.add(close_button,3,3);
+    gridPane.setMinSize(600,300);        
+    gridPane.setVgap(10);        
+    gridPane.setHgap(20);        
+    gridPane.setAlignment(Pos.CENTER); 
+    
+    //SCENE
+    Scene my_scene = new Scene(gridPane);
+
+
+    //STAGE
+    primaryStage.setScene(my_scene);
+    primaryStage.setTitle("Exprobe Networks Limited");
+    primaryStage.show();    
+        
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/exprobenetworks/Register.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exprobenetworks;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class Register extends Application{
+    
+    //create a new stage for registration form
+    Stage register_page = new Stage();
+    
+    @Override
+    public void start(Stage primaryStage){
+     
+        Text username_label = new Text("Username: ");
+        Text email_label = new Text("Email Address: ");
+        Text password_label = new Text("SetPassword: ");
+        Text printoutQry = new Text();
+        
+        Button register_button = new Button("Register");
+        Button close_button = new Button("Close");
+        
+        TextField username = new TextField();
+        TextField email = new TextField();
+        PasswordField password = new PasswordField();
+        
+        
+        GridPane gridPane = new GridPane();
+        //Label Fields 
+        gridPane.add(username_label, 0,0);
+        gridPane.add(email_label, 0,1);
+        gridPane.add(password_label, 0,2);
+        //Text Fields
+        gridPane.add(username, 1,0);
+        gridPane.add(email, 1,1);
+        gridPane.add(password, 1,2);
+        gridPane.add(register_button, 1,3);
+        gridPane.add(close_button, 2,3);
+        gridPane.add(printoutQry, 1,4);
+        
+        
+        
+        
+        gridPane.setMinSize(500,250);
+        gridPane.setVgap(10);
+        gridPane.setHgap(20);
+        gridPane.setAlignment(Pos.CENTER);
+        
+        
+        //EventHandler for close button
+        close_button.setOnMouseClicked((new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent event){
+                primaryStage.close();
+            }
+        }));
+        
+        
+        //REGISTER AND INSERT INFORMATION TO THE DATABASE
+        register_button.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent event){
+                final String username1 = username.getText();
+                final String email1 = email.getText();
+                final String password1 = password.getText();
+                
+                try
+                {
+                    //step one
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    
+                    //step two
+                    Connection con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/exprobe_networks", "root","");
+                    
+                    //step three
+                    Statement stmt = con.createStatement();
+                    
+                    //step four
+                    String mySQL_stmt = "INSERT INTO `users`(`username`,`email`, `password`) VALUES ("+"'"+username1+"'"+","+"'"+email1+"'"+","+"'"+password1+"'"+")";
+                    System.out.println(mySQL_stmt);
+                    stmt.executeUpdate(mySQL_stmt);
+                    
+                    //step five
+                    con.close();
+                    printoutQry.setText("Record Successfully Saved!");
+                }
+                catch(Exception ee){
+                    System.out.println("ee");printoutQry.setText("Record NOT SAVED!");;
+                }
+                
+            }
+        });
+        
+        // SCENE
+        Scene my_scene = new Scene(gridPane);
+        gridPane.setStyle("-fx-background-color: #9B9B9B; ");
+        primaryStage.setScene(my_scene);
+        primaryStage.setTitle("Welcome to Exprobe Networks Limited!");
+        primaryStage.show();      
+               
+      
+    }
+}
+
